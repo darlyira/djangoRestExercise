386a553aec638c6374703d7ed1fa643816dc08bb
darlyira <irakozechristadarlene@gmail.com>
2023-12-01 20:57:10 +0200

    using viewset

--- api/urls.py
+++ api/urls.py
@@ -6,19 +6,9 @@ app_name= "socialnetwork"
 route = DefaultRouter()
 route.register('user',views.UserViewset)
 route.register('comment',views.CommentViewset)
+route.register('posts',views.PostViewset)
 
 urlpatterns = [
     path('',include(route.urls)),
-    # path('user/list',views.getUsers),
-    # path('user/delete/<int:id>',views.deleteUser),
-    # path('post/delete/<int:id>',views.delete_post),
-    # path('comment/delete/<int:id>',views.delete_comment),
-    # path('post/add',views.new_post),
-    # path('user/edit/<int:id>',views.editUser),
-    # path('post/edit/<int:id>',views.edit_post),
-    # path('comment/edit/<int:id>',views.edit_comment),
-    # path('post/list',views.get_post),
-    # path('comment/add',views.new_comment),
-    # path('comment/list',views.get_comment)
 
 ]
