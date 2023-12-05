af272ab9f49f9127a50ab1fd5504d3b68adbae56
darlyira <145869499+darlyira@users.noreply.github.com>
2023-12-05 21:29:06 +0200

    Update serializer.py

--- api/serializer.py
+++ api/serializer.py
@@ -1,63 +1,25 @@
 from rest_framework import serializers
+from socialnetwork.models import User,Posts,Comment
 
-from .models.publications import (
-    ErpIncidentReport,
-    ErpPublication,
-    ErpPublicationCategorie,
-    ErpPublicationComment,
-    ErpPublicationReaction,
-    ErpPublicationTag,
-    ErpPublicationType,
-)
-
-
-class ErpPublicationTypeSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = ErpPublicationType
-        fields = "__all__"
+        model = User
+        fields = ['name']
 
+class UserdetaillsSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = User
+        fields = '__all__'
 
-class ErpPublicationCategorieSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ErpPublicationCategorie
-        fields = "__all__"
+        model = Posts
+        fields = ['id','title','contexte']
+        
 
-
-class ErpPublicationSerializer(serializers.ModelSerializer):
+class CommentSerializer(serializers.HyperlinkedModelSerializer):
+    user= UserSerializer()
+    post = PostSerializer()
     class Meta:
-        model = ErpPublication
-        fields = "__all__"
-
-
-class ErpPublicationCommentSerialiser(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = ErpPublicationComment
-        fields = "__all__"
-
-
-class ErpIncidentReportSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ErpIncidentReport
-        fields = "__all__"
-
-
-class ErpPublicationReactionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ErpPublicationReaction
-        fields = "__all__"
-
-
-class ErpPublicationTagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ErpPublicationTag
-        fields = "__all__"
-
-
-class ErpPublicationDetaillsSerializer(serializers.HyperlinkedModelSerializer):
-    category = ErpPublicationCategorieSerializer()
-    publicaion_type = ErpPublicationTypeSerializer()
-    comments = ErpPublicationCommentSerialiser()
-
-    class Meta:
-        model = ErpPublication
-        fields = ["title", "client", "codument", "co_publisher", "caption", "publication_type", "category", "comments"]
+        model = Comment
+        fields = ['content','created_at','updated_at','user','post']
